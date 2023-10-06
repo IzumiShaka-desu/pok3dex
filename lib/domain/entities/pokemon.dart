a0a170f26667757054e346e6bb181af83b55c1c9
IzumiShaka-desu <shakaaji29@gmail.com>
2023-10-06 14:51:44 +0700

    feat: add  pokemon entity

---
+++ lib/domain/entities/pokemon.dart
@@ -0,0 +1,43 @@
+class Pokemon {
+  final int id;
+  final String pokemon;
+  final String type;
+  final List<String> abilities;
+  final int hitpoints;
+  final List<String> evolutions;
+  final String location;
+  final String imageUrl;
+
+  Pokemon({
+    required this.id,
+    required this.pokemon,
+    required this.type,
+    required this.abilities,
+    required this.hitpoints,
+    required this.evolutions,
+    required this.location,
+    required this.imageUrl,
+  });
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) => Pokemon(
+        id: json["id"],
+        pokemon: json["pokemon"],
+        type: json["type"],
+        abilities: List<String>.from(json["abilities"].map((x) => x)),
+        hitpoints: json["hitpoints"],
+        evolutions: List<String>.from(json["evolutions"].map((x) => x)),
+        location: json["location"],
+        imageUrl: json["image_url"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "pokemon": pokemon,
+        "type": type,
+        "abilities": List<dynamic>.from(abilities.map((x) => x)),
+        "hitpoints": hitpoints,
+        "evolutions": List<dynamic>.from(evolutions.map((x) => x)),
+        "location": location,
+        "image_url": imageUrl,
+      };
+}
