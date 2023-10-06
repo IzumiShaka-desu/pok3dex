27a9ee6ca78c8e3010e2ef775b8615f0bc337a8d
IzumiShaka-desu <shakaaji29@gmail.com>
2023-10-06 17:15:42 +0700

    feat: add constants

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,3 +1,5 @@
 abstract class Constants {
-  static const String baseUrl = 'https://dummyapi.online/api/pokemon';
+  static const String baseUrl = 'https://dummyapi.online/api';
+  static const String baseUrlPngImages =
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/';
 }
